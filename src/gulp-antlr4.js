29edc73c0080f7ad5e4968f9de824db055ad6625
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-01 15:44:39 +0200

    Generate parser only for g4 files
    Also made error messages more explicit.

--- src/gulp-antlr4.js
+++ src/gulp-antlr4.js
@@ -1,31 +1,34 @@
 import {PluginError} from 'gulp-util';
 import through from 'through2';
+import path from 'path';
 import {spawn} from 'child_process';
 import childProcessData from 'child-process-data';
 
 const PLUGIN_NAME = 'gulp-antlr4';
 
-export default function (_outputDir) {
-  const outputDir = _outputDir && _outputDir.output || _outputDir;
+export default function (_antlrDir) {
+  const antlrDir = _antlrDir && _antlrDir.dir || _antlrDir;
 
-  if (typeof outputDir !== 'string') {
+  if (typeof antlrDir !== 'string') {
     throw new PluginError(PLUGIN_NAME,
-      new TypeError(`Requires a proper output option`));
+      new TypeError(`You must provide the directory where to write or read
+     the generated lexing and parsing tools (option 'dir')`));
   }
 
   const CLASSPATH = process.env.CLASSPATH;
 
   if (!CLASSPATH) {
     throw new PluginError(PLUGIN_NAME,
-      new ReferenceError(`$CLASSPATH is not defined`));
+      new ReferenceError(`Environment variable CLASSPATH is not defined`));
   }
 
+  // Not matching '~' as it is not understood by Java anyway
   const matchJar = CLASSPATH.match(
-    /.*:((\d|\w|\/|-|_|\.|\~)+antlr-\d+\.\d+-complete\.jar):.*/);
+    /.*:((\d|\w|\/|-|_|\.)+antlr-\d+\.\d+-complete\.jar):.*/);
 
   if (matchJar === null) {
     throw new PluginError(PLUGIN_NAME,
-      new ReferenceError(`Cannot find ANTLR jar file`));
+      new ReferenceError(`Cannot find ANTLR 4 .jar file`));
   }
 
   return through.obj(function (file, encoding, callback) {
@@ -37,14 +40,22 @@ export default function (_outputDir) {
       this.emit('error', new PluginError(PLUGIN_NAME,
         'Streams not supported!'));
     } else if (file.isBuffer()) {
-      childProcessData(spawn('java', ['org.antlr.v4.Tool',
-        '-Dlanguage=JavaScript', '-o', outputDir, file.history[0]]))
-      .then(() => {
-        callback(null, file);
-      }, err => {
-        this.emit('error', new PluginError(PLUGIN_NAME,
-          new Error(err)));
-      });
+      const inputFile = file.history[0];
+
+      switch (path.extname(inputFile)) {
+      case '.g4':
+        return childProcessData(spawn('java', ['org.antlr.v4.Tool',
+          '-Dlanguage=JavaScript', '-o', antlrDir, inputFile]))
+        .then(() => {
+          callback(null, file);
+        }, err => {
+          this.emit('error', new PluginError(PLUGIN_NAME,
+            new Error(err)));
+        });
+
+      default:
+        return callback(null, file);
+      }
     }
   });
 }
