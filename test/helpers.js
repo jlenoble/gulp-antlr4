2bf5e9f0ca16de7d97634a355d9929d232a9eb5c
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-01 14:11:30 +0200

    Made outputDir optional
    using option 'outputDir' in testGrammar

--- test/helpers.js
+++ test/helpers.js
@@ -4,8 +4,12 @@ export const outputDir = 'build/antlr4';
 
 export const testGrammar = _options => {
   const options = Object.assign({
-    childProcess: ['gulp', ['--gulpfile',
-      'test/gulpfiles/gulpfile.babel.js', `--grammar=${_options.grammarName}`]],
+    childProcess: ['gulp', [
+      '--gulpfile',
+      'test/gulpfiles/gulpfile.babel.js',
+      `--grammar=${_options.grammarName}`,
+      `--output=${_options.outputDir || outputDir}`,
+    ]],
   }, _options);
 
   const test = makeSingleTest(options);
