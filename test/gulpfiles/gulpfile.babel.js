1fce1f288521279f4f0a78b0ef6c455b926094b6
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-01 16:00:14 +0200

    'run' task
    Defined global consts sourcesDir, grammarGlob, antlrDir and inputGlob.

--- test/gulpfiles/gulpfile.babel.js
+++ test/gulpfiles/gulpfile.babel.js
@@ -2,11 +2,20 @@ import gulp from 'gulp';
 import antlr4 from '../../src/gulp-antlr4.js';
 import {argv} from 'yargs';
 
-const test = () => {
-  return gulp.src([
-    `../sources/**/${argv.grammar || '*'}.g4`,
-  ])
-    .pipe(antlr4('../../' + `${argv.output || 'build/antlr4'}`));
+const sourcesDir = '../sources';
+const grammarGlob = [`${sourcesDir}/${argv.grammar || '*'}.g4`];
+const antlrDir = `../../${argv.output || 'build/antlr4'}`;
+const inputGlob = [`${sourcesDir}/${argv.input || '*.txt'}`];
+
+const generate = () => {
+  return gulp.src(grammarGlob, {since: gulp.lastRun(generate)})
+    .pipe(antlr4(antlrDir));
 };
 
-gulp.task('default', test);
+const run = () => {
+  return gulp.src(inputGlob, {since: gulp.lastRun(run)})
+    .pipe(antlr4(antlrDir));
+};
+
+gulp.task('run', gulp.series(generate, run));
+gulp.task('default', generate);
