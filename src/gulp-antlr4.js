66a8a1f52accde44cacd3584d7c81f5b62c85ba8
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-31 18:15:26 +0200

    Factored out consumeData
    Planning on reusing it in the 'isStream' case

--- src/gulp-antlr4.js
+++ src/gulp-antlr4.js
@@ -49,50 +49,8 @@ export default function (_options) {
       default:
         if (ANTLR4.isProperlySetup()) {
           const data = file.contents.toString('utf8');
-          const chars = new InputStream(data, true);
-          const lexer = new ANTLR4.Lexer(chars);
-          const tokens = new CommonTokenStream(lexer);
-          const parser = new ANTLR4.Parser(tokens);
-          parser.buildParseTrees = true;
-          const tree = parser[ANTLR4.rule]();
-
-          switch (mode) {
-          case 'tree':
-            console.log(tree.toStringTree(parser.ruleNames));
-            break;
-
-          case 'listener':
-            new Promise((resolve, reject) => {
-              const walker = new ParseTreeWalker();
-              const listener = new ANTLR4.Listener(resolve, reject);
-              walker.walk(listener, tree);
-            }).then(() => {
-              this.emit('finish');
-            }, err => {
-              this.emit('error', new PluginError(PLUGIN_NAME, err));
-            });
-            return;
-
-          case 'visitor':
-            try {
-              const visitor = new ANTLR4.Visitor();
-
-              Promise
-                .resolve(visitor.visit(tree))
-                .then(() => {
-                  this.emit('finish');
-                });
-
-              return; // Don't use callback but rely on above Promise
-              // to emit eventually the proper 'finish' event
-            } catch (err) {
-              this.emit('error', new PluginError(PLUGIN_NAME, err));
-            }
-            break;
-          }
+          consumeData({data, ANTLR4, mode, ctx: this});
         }
-
-        return callback(null, file);
       }
     }
   });
@@ -243,4 +201,48 @@ function requireAfresh (name, dir) {
   }
 
   return require(file)[name];
+}
+
+function consumeData ({data, ANTLR4, ctx, mode}) {
+  const chars = new InputStream(data, true);
+  const lexer = new ANTLR4.Lexer(chars);
+  const tokens = new CommonTokenStream(lexer);
+  const parser = new ANTLR4.Parser(tokens);
+  parser.buildParseTrees = true;
+  const tree = parser[ANTLR4.rule]();
+
+  switch (mode) {
+  case 'tree':
+    console.log(tree.toStringTree(parser.ruleNames));
+    return;
+
+  case 'listener':
+    new Promise((resolve, reject) => {
+      const walker = new ParseTreeWalker();
+      const listener = new ANTLR4.Listener(resolve, reject);
+      walker.walk(listener, tree);
+    }).then(() => {
+      ctx.emit('finish');
+    }, err => {
+      ctx.emit('error', new PluginError(PLUGIN_NAME, err));
+    });
+    return;
+
+  case 'visitor':
+    try {
+      const visitor = new ANTLR4.Visitor();
+
+      Promise
+        .resolve(visitor.visit(tree))
+        .then(() => {
+          ctx.emit('finish');
+        });
+
+      return; // Don't use callback but rely on above Promise
+      // to emit eventually the proper 'finish' event
+    } catch (err) {
+      ctx.emit('error', new PluginError(PLUGIN_NAME, err));
+    }
+    break;
+  }
 }
