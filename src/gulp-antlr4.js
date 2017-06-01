f40279fce9ece5d86448da09bf8e2bdbe52e7782
Jason Lenoble <jason.lenoble@gmail.com>
2017-06-01 21:26:08 +0200

    Factored out consumeData

--- src/gulp-antlr4.js
+++ src/gulp-antlr4.js
@@ -1,15 +1,10 @@
-/* import toArray from 'stream-to-array';
-import {Buffer} from 'buffer';*/
 import {PluginError} from 'gulp-util';
 import through from 'through2';
 import path from 'path';
-import {InputStream, CommonTokenStream} from 'antlr4';
-import {ParseTreeWalker} from 'antlr4/tree';
 import checkJava from './check-java';
 import formatOptions from './format-options';
 import makeParser from './make-parser';
-
-const PLUGIN_NAME = 'gulp-antlr4';
+import consumeData from './consume-data';
 
 export default function (options) {
   checkJava();
@@ -62,48 +57,4 @@ export default function (options) {
       }
     }
   });
-}
-
-function consumeData ({data, ANTLR4, ctx, mode}) {
-  const chars = new InputStream(data, true);
-  const lexer = new ANTLR4.Lexer(chars);
-  const tokens = new CommonTokenStream(lexer);
-  const parser = new ANTLR4.Parser(tokens);
-  parser.buildParseTrees = true;
-  const tree = parser[ANTLR4.rule]();
-
-  switch (mode) {
-  case 'tree':
-    console.log(tree.toStringTree(parser.ruleNames));
-    return;
-
-  case 'listener':
-    new Promise((resolve, reject) => {
-      const walker = new ParseTreeWalker();
-      const listener = new ANTLR4.Listener(resolve, reject);
-      walker.walk(listener, tree);
-    }).then(() => {
-      ctx.emit('finish');
-    }, err => {
-      ctx.emit('error', new PluginError(PLUGIN_NAME, err));
-    });
-    return;
-
-  case 'visitor':
-    try {
-      const visitor = new ANTLR4.Visitor();
-
-      Promise
-        .resolve(visitor.visit(tree))
-        .then(() => {
-          ctx.emit('finish');
-        });
-
-      return; // Don't use callback but rely on above Promise
-      // to emit eventually the proper 'finish' event
-    } catch (err) {
-      ctx.emit('error', new PluginError(PLUGIN_NAME, err));
-    }
-    break;
-  }
 }
